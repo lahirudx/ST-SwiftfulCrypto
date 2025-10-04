f7dd9a26484cc4dfee15c39d1d01897803b04391
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-10-04 15:07:52 +0530

    9. Saving images to the File Manager

--- SwiftfulCrypto/Services/CoinImageService.swift
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -14,19 +14,41 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     
     private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName: String = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
-        getCoinImage(coin: coin)
+        self.coin = coin
+        self.imageName = coin.id
+        getCoinImage()
     }
     
-    private func getCoinImage(coin: CoinModel) {
+    private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Returning Coin Image From Cache: \(coin.name)")
+        } else {
+            downloadCoinImage(coin: coin)
+            print("Downloading Coin Image: \(coin.name)")
+        }
+    }
+    
+    
+    
+    private func downloadCoinImage(coin: CoinModel) {
+        
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription =  NetworkingManager.download(url: url)
             .tryMap({ UIImage(data: $0)} )
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: imageName, folderName: self.folderName)
             })
     }
 }
